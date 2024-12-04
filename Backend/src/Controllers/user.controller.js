aedc4fad8e6d2c2ba73667da5f77ef9f4b8ac32c
sheraz61 <hsheraz271@gmail.com>
2024-12-04 23:00:31 +0500

    almost complete user controller

--- Backend/src/Controllers/user.controller.js
+++ Backend/src/Controllers/user.controller.js
@@ -184,49 +184,183 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 
 //Change Password
 
-const changePassword= asyncHandler(async(req,res)=>{
-    const {oldPassword, newPassword} = req.body;
+const changePassword = asyncHandler(async (req, res) => {
+    const { oldPassword, newPassword } = req.body;
     const user = await User.findById(req.user?._id)
-    if(!user){
-        throw new apiError(401,"User not found")
+    if (!user) {
+        throw new apiError(401, "User not found")
     }
     const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
-    if(!isPasswordCorrect){
-        throw new apiError(400,"Incorrect old password")
+    if (!isPasswordCorrect) {
+        throw new apiError(400, "Incorrect old password")
     }
-    user.password= newPassword
-    await user.save({validateBeforeSave:false})
+    user.password = newPassword
+    await user.save({ validateBeforeSave: false })
 
     return res.status(200).json(
-        new apiResponse(200,{},"Password changed successfully")
+        new apiResponse(200, {}, "Password changed successfully")
     )
 })
 
 //get Current User
 
-const getCurrentUser= asyncHandler(async(req, res)=>{
+const getCurrentUser = asyncHandler(async (req, res) => {
     return res.status(200).json(
-        new apiResponse(200,req.user,"User retrieved successfully")
+        new apiResponse(200, req.user, "User retrieved successfully")
     )
 })
 
 
 //updata Account Details
 
-const updateAccountDetails= asyncHandler(async(req,res)=>{
-    const {fullName,email } =req.body
-if(!fullName || !email){
-    throw new apiError(400,"Full Name and Email are required")
-}
-const user = await User.findOneAndUpdate(req.user?._id,{
-    $set:{
-        fullName,
-        email
+const updateAccountDetails = asyncHandler(async (req, res) => {
+    const { fullName, email } = req.body
+    if (!fullName || !email) {
+        throw new apiError(400, "Full Name and Email are required")
     }
-},
-{new:true}
-).select("-password")
-return res.status(200).json(
-    new apiResponse(200,user,"User details updated successfully")
-)
+    const user = await User.findOneAndUpdate(req.user?._id, {
+        $set: {
+            fullName,
+            email
+        }
+    },
+        { new: true }
+    ).select("-password")
+    return res.status(200).json(
+        new apiResponse(200, user, "User details updated successfully")
+    )
 })
+
+// update user Profile Image
+
+const updateUserProfileImage = asyncHandler(async (req, res) => {
+    const profileImageLocalPath = req.file?.path
+    if (!profileImageLocalPath) {
+        throw new apiError(400, "Profile image not found")
+    }
+    const userForProfileImage = await User.findById(req.user?._id)
+    if (!userForProfileImage) {
+        throw new apiError(400, "User not found")
+    }
+    const oldProfileImage = await deleteInCloudinary(userForProfileImage.profileImage)
+    if (!oldProfileImage) {
+        throw new apiError(500, "Failed to delete old profile image")
+    }
+    const profileImg = await uploadOnCloudinary(profileImageLocalPath)
+    if (!profileImg) {
+        throw new apiError(500, "Failed to upload profile image")
+    }
+    const user = await User.findByIdAndUpdate(req.user?._id, {
+        $set: {
+            profileImage: profileImg.url
+        }
+    }, {
+        new: ture
+    }).select("-password")
+    return res.status(200).json(
+        new apiError(200, user, "Profile updated Successfully")
+    )
+})
+
+
+// update cover Image
+
+const updateCoverImage = asyncHandler(async (req, res) => {
+    const coverImageLocalPath = req.file?.path
+    if (!coverImageLocalPath) {
+        throw new apiError(400, "Cover image not found")
+    }
+    const userForCoverImage = await User.findById(req.user?._id)
+    if (!userForCoverImage) {
+        throw new apiError(400, "User not found")
+    }
+    const oldCoverImage = await deleteInCloudinary(userForCoverImage.coverImage)
+    if (!oldCoverImage) {
+        throw new apiError(500, "Failed to delete old cover image")
+    }
+    const coverImg = await uploadOnCloudinary(coverImageLocalPath)
+    if (!coverImg.url) {
+        throw new apiError(500, "Failed to upload cover image")
+    }
+    const user = await User.findByIdAndUpdate(req.user?._id, {
+        $set: {
+            coverImage: coverImg.url
+        }
+    }, {
+        new: true
+    }).select("-password")
+    return res.status(200).json(
+        new apiResponse(200, user, "Cover updated successfully")
+    )
+}) 
+
+//get user profile
+
+const getUserProfile = asyncHandler(async (req, res) => {
+    const { userName } = req.params
+    if (!userName?.trim()) {
+      throw new apiError(400, 'Username is required')
+    }
+  
+    const channel = await User.aggregate([
+      {
+        $match: {
+          userName: userName?.toLowerCase()
+        }
+      },
+      {
+        $lookup: {
+          from: "subscriptions",
+          localField: "_id",
+          foreignField: "channel",
+          as: "subscribers"
+        }
+      },
+      {
+        $lookup: {
+          from: "subscriptions",
+          localField: "_id",
+          foreignField: "subscriber",
+          as: "subscribedTo"
+        }
+      },
+      {
+        $addFields: {
+          subscriberCount: {
+            $size: "$subscribers"
+          },
+          channelSubscribedToCount: {
+            $size: "$subscribedTo"
+          },
+          isSubscribed: {
+            $cond: {
+              if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+              then: true,
+              else: false
+            }
+          }
+        }
+      },
+      {
+        $project: {
+          fullName: 1,
+          userName: 1,
+          profileImage: 1,
+          coverImage: 1,
+          subscriberCount: 1,
+          channelSubscribedToCount: 1,
+          isSubscribed: 1,
+          email: 1,
+  
+        }
+      }
+    ])
+    if (!channel?.length) {
+      throw new apiError(404, "channel does not exist")
+    }
+  
+    return res.status(200)
+      .json(
+        new apiResponse(200, channel[0], "User channel fetched successfully")
+      )
+  })
